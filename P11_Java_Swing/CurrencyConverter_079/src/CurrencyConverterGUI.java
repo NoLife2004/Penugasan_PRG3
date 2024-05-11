34e5eb0184190465bdce5a4d868c06cb6c81bf7c
NoLife2004 <152877445+NoLife2004@users.noreply.github.com>
2024-05-11 09:46:39 +0700

    Add some convert option

--- P11_Java_Swing/CurrencyConverter_079/src/CurrencyConverterGUI.java
+++ P11_Java_Swing/CurrencyConverter_079/src/CurrencyConverterGUI.java
@@ -36,6 +36,24 @@ public class CurrencyConverterGUI {
                     convertRupiahBaht();
                 } else if (MataUangAwal.getSelectedItem() == "Rupiah" && MataUangAkhir.getSelectedItem() == "Dollar"){
                     convertRupiahDollar();
+                } else if (MataUangAwal.getSelectedItem() == "Ringgit" && MataUangAkhir.getSelectedItem() == "Rupiah"){
+                    convertRinggitRupiah();
+                } else if (MataUangAwal.getSelectedItem() == "Ringgit" && MataUangAkhir.getSelectedItem() == "Baht"){
+                    convertRinggitBaht();
+                } else if (MataUangAwal.getSelectedItem() == "Ringgit" && MataUangAkhir.getSelectedItem() == "Dollar"){
+                    convertRinggitDollar();
+                } else if (MataUangAwal.getSelectedItem() == "Baht" && MataUangAkhir.getSelectedItem() == "Ringgit"){
+                    convertBahtRinggit();
+                } else if (MataUangAwal.getSelectedItem() == "Baht" && MataUangAkhir.getSelectedItem() == "Rupiah"){
+                    convertBahtRupiah();
+                } else if (MataUangAwal.getSelectedItem() == "Baht" && MataUangAkhir.getSelectedItem() == "Dollar"){
+                    convertBahtDollar();
+                } else if (MataUangAwal.getSelectedItem() == "Dollar" && MataUangAkhir.getSelectedItem() == "Ringgit"){
+                    convertDollarRinggit();
+                } else if (MataUangAwal.getSelectedItem() == "Dollar" && MataUangAkhir.getSelectedItem() == "Rupiah"){
+                    convertDollarRupiah();
+                } else if (MataUangAwal.getSelectedItem() == "Dollar" && MataUangAkhir.getSelectedItem() == "Baht"){
+                    convertDollarBaht();
                 }
             }
         });
@@ -54,6 +72,51 @@ public class CurrencyConverterGUI {
     public void convertRupiahDollar(){
         float RupiahDollar = (float) ((Double.parseDouble(JumlahUang.getText())) * 0.000062);
         ConvertedLabel.setText(String.format("%.2f", RupiahDollar));
+    }
+
+    public void convertRinggitRupiah(){
+        float RinggitRupiah = (float) ((Double.parseDouble(JumlahUang.getText())) * 3388.92);
+        ConvertedLabel.setText(String.format("%.2f", RinggitRupiah));
+    }
+
+    public void convertRinggitBaht(){
+        float RinggitBaht = (float) ((Double.parseDouble(JumlahUang.getText())) * 7.73);
+        ConvertedLabel.setText(String.format("%.2f", RinggitBaht));
+    }
+
+    public void convertRinggitDollar(){
+        float RinggitDollar = (float) ((Double.parseDouble(JumlahUang.getText())) * 0.21);
+        ConvertedLabel.setText(String.format("%.2f", RinggitDollar));
+    }
+
+    public void convertBahtRupiah(){
+        float BahtRupiah = (float) ((Double.parseDouble(JumlahUang.getText())) * 437.94);
+        ConvertedLabel.setText(String.format("%.2f", BahtRupiah));
+    }
+
+    public void convertBahtRinggit(){
+        float BahtRinggit = (float) ((Double.parseDouble(JumlahUang.getText())) * 0.12878);
+        ConvertedLabel.setText(String.format("%.2f", BahtRinggit));
+    }
+
+    public void convertBahtDollar(){
+        float BahtDollar = (float) ((Double.parseDouble(JumlahUang.getText())) * 0.027);
+        ConvertedLabel.setText(String.format("%.2f", BahtDollar));
+    }
+
+    public void convertDollarRupiah(){
+        float DollarRupiah = (float) ((Double.parseDouble(JumlahUang.getText())) * 16055);
+        ConvertedLabel.setText(String.format("%.2f", DollarRupiah));
+    }
+
+    public void convertDollarRinggit(){
+        float DollarRinggit = (float) ((Double.parseDouble(JumlahUang.getText())) * 4.74);
+        ConvertedLabel.setText(String.format("%.2f", DollarRinggit));
+    }
+
+    public void convertDollarBaht(){
+        float DollarBaht = (float) ((Double.parseDouble(JumlahUang.getText())) * 36.66);
+        ConvertedLabel.setText(String.format("%.2f", DollarBaht));
     }
 
     public static void main(String[] args) {
